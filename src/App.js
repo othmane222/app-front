29a7e84aa9b65bdce92d74e68c45cc77fa6d9b54
Issam Bekkari <bekkariissam1999@gmail.com>
2024-09-05 20:06:33 +0100

    Added courses and course-details page alongside the video player

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Routes, Route, Router, redirect, Navigate} from 'react-router-dom';
+import { Routes, Route, Router, redirect, Navigate } from 'react-router-dom';
 import HomePage from './components/Home/Home';
 import LoginSignup from './components/LoginSignup/LoginSignup';
 import ResetPassword from './components/Resetpassword/ResetPassword';
@@ -14,37 +14,46 @@ import Profile from "./components/Profile";
 import ManageStudents from "./components/ManageStudents";
 import ManageCategories from "./components/ManageCategories";
 import ManageTeachers from "./components/ManageTeachers";
+import CourseGrid from './components/Courses/CourseGrid';
+import CourseDetails from './components/Courses/CourseDetails';
+import VideoDetails from './components/Courses/VideoDetails';
+import VideoPlayer from './components/Courses/VideoPlayer';
 
 // Other imports...
 
 const App = () => {
   return (
-      <AuthProvider>
-    <Routes>
-      <Route path="/" element={<LoginSignup />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path='/categories' element={<Categories/>} />
-      <Route path='/cart' element={<Cart/>} />
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<LoginSignup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path='/categories' element={<Categories />} />
+        <Route path='/cart' element={<Cart />} />
 
         <Route element={<PrivateRoute />}>
-        <Route path="/board" element={<DashBoard />} />
+          <Route path="/board" element={<DashBoard />} />
         </Route>
 
 
-      <Route path="admin"  element={<Admin />}>
-        <Route path="" action={() => <Navigate to={"./profile"} />} loader={() => <Navigate to={"./profile"} />} />
-      <Route path="manage-users" element={<ManageStudents />} />
-      <Route path="manage-categories" element={<ManageCategories />} />
-      <Route path="manage-courses" element={<ManageTeachers />} />
+        <Route path="admin" element={<Admin />}>
+          <Route path="" action={() => <Navigate to={"./profile"} />} loader={() => <Navigate to={"./profile"} />} />
+          <Route path="manage-users" element={<ManageStudents />} />
+          <Route path="manage-categories" element={<ManageCategories />} />
+          <Route path="manage-courses" element={<ManageTeachers />} />
+        </Route>
 
-      </Route>
+        // Course routes
+        <Route path="/courses" element={<CourseGrid />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/course/:courseId/video/:videoId" element={<VideoDetails />} />
+        <Route path="/video/:videoId" element={<VideoPlayer />} />
 
 
 
-    </Routes>
-      </AuthProvider>
+      </Routes>
+    </AuthProvider>
   );
 };
 
